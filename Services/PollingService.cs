45b1e33d53c84fa04fa553181a0d16deff75be36
Oleksii Zhyglov <oleksiizhyglov@gmail.com>
2023-01-29 01:52:36 +0200

    added all files after gitignore to be sure to hide secrets

---
+++ Services/PollingService.cs
@@ -0,0 +1,15 @@
+using Telegram.Bot.Abstract;
+
+namespace Telegram.Bot.Services
+{
+
+    // Compose Polling and ReceiverService implementations
+    public class PollingService : PollingServiceBase<ReceiverService>
+    {
+        public PollingService(IServiceProvider serviceProvider, ILogger<PollingService> logger)
+            : base(serviceProvider, logger)
+        {
+        }
+    }
+}
+
